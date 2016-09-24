01cdc6b938f5ad11644a7549994067c2f212533e
harshitha97 <h.puranik25@gmail.com>
2016-09-24 17:54:11 +0530

    commited

---
+++ app/src/main/java/com/example/android/happybirthady/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.happybirthady;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
